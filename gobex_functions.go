9e7baa33e17e51d49b6aba662f05df7c25721577
Alexandra Pereira <alesilva241@gmail.com>
2022-08-14 13:55:28 -0400

    2day-multiple_return_functions

--- gobex_functions.go
+++ gobex_functions.go
@@ -10,10 +10,30 @@ func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
+//returning multiples values
+
+func vals(a, b int) (int, int) {
+	return a + b, a * b
+}
+
 func main() {
 	res := plus(1, 2)
 	fmt.Println("1+2 =", res)
 
 	res = plusPlus(1, 2, 3)
 	fmt.Println("1+2+3 =", res)
+
+	a, b := vals(3, 2)
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c, _ := vals(1, 4)
+	fmt.Println(c)
+
+	_, d := vals(5, 3)
+	fmt.Println(d)
+
+	c, d = vals(2, 3)
+	fmt.Println(c, d)
+
 }
