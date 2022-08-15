3816a55a3601c36a9fc060cb852f0370a7451bfc
Alexandra Pereira <alesilva241@gmail.com>
2022-08-15 00:50:10 -0400

    3day-closures+recursives_in_golang

--- gobex_closures.go
+++ gobex_closures.go
@@ -10,6 +10,15 @@ func intSeq() func() int {
 	}
 }
 
+// append recursive prog example
+// classicals factorial and fibonacci
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
 func main() {
 	nextInt := intSeq()
 
@@ -18,4 +27,17 @@ func main() {
 
 	newInts := intSeq()
 	fmt.Println(newInts())
+
+	fmt.Println(fact(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
